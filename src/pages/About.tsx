8abfa20e3b53a694f91fc211c0c2c45bad33d709
WesleyEdwards <westheedwards@gmail.com>
2023-11-01 09:59:43 -0600

    changing assets location

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,13 @@
 import { useMediaQuery } from "@mui/material";
 import "./About.css";
+import wesleyPicture from "../assets/wesley-picture.jpg";
+import inMexico from "../assets/in-mexico.jpg";
+import atHackathon from "../assets/at-hackathon.jpg";
+import skiing from "../assets/skiing.png";
+import marathon from "../assets/marathon.jpg";
+import hockey from "../assets/hockey.jpg";
+import mate from "../assets/mate.jpg";
+
 export const About = () => {
   const smallScreen = useMediaQuery("(max-width: 600px)");
 
@@ -9,21 +17,21 @@ export const About = () => {
   return (
     <div className="container about-page">
       <div id="profile-pic" className="col-flex">
-        <img src="../public/wesley-picture.jpg" className="full-width" />
+        <img src={wesleyPicture} className="full-width" />
         <div>
           <p className="quote">"God, Family, Unix"</p>
           <p className="author">- Wesley Edwards</p>
         </div>
       </div>
       <div className={`${smallScreenStyle("col-flex", "row-flex")} full-width`}>
-        <img src="../public/in-mexico.jpg" className="half-width" />
+        <img src={inMexico} className="half-width" />
         <p className={smallScreenStyle("", "horizontal-text")}>
           Wesley and his wife Sarah live in Logan, Utah. They love back-packing,
           running, attending Utah State sports events, and being with family.
         </p>
       </div>
       <div>
-        <img src="../public/at-hackathon.jpg" className="full-width" />
+        <img src={atHackathon} className="full-width" />
         <p>
           The computer science department at Utah State University sponsors many
           events and clubs, including the annual hackathon the ACM club, both of
@@ -33,10 +41,10 @@ export const About = () => {
 
       <div>
         <div className="pictures">
-          <img src="../public/skiing.png" className="half-width" />
+          <img src={skiing} className="half-width" />
           <div className={"col-flex pictures-col"}>
-            <img src="../public/marathon.jpg" className="full-width" />
-            <img src="../public/hockey.jpg" className="full-width" />
+            <img src={marathon} className="full-width" />
+            <img src={hockey} className="full-width" />
           </div>
         </div>
         <p>
@@ -54,14 +62,20 @@ export const About = () => {
         <p className={smallScreenStyle("", "horizontal-text")}>
           Yerba Mate is a traditional South American drink that has inspired
           several school and personal projects for Wesley, such as{" "}
-          <a target="_blank" href="https://github.com/WesleyEdwards/GoMatero">Go Matero</a> and{" "}
-          <a target="_blank" href="https://github.com/WesleyEdwards/mate-champion">
+          <a target="_blank" href="https://github.com/WesleyEdwards/GoMatero">
+            Go Matero
+          </a>{" "}
+          and{" "}
+          <a
+            target="_blank"
+            href="https://github.com/WesleyEdwards/mate-champion"
+          >
             Mate Champion
           </a>
           .
         </p>
 
-        <img src="../public/mate.jpg" className="half-width" />
+        <img src={mate} className="half-width" />
       </div>
     </div>
   );
--- src/utils/projectData.ts
+++ src/utils/projectData.ts
@@ -1,3 +1,19 @@
+import reactLogo from "../assets/react-logo.png";
+import firebaseLogo from "../assets/firebase-logo.png";
+import tsLogo from "../assets/ts-logo.png";
+import muiLogo from "../assets/mui-logo.png";
+import jetpackLogo from "../assets/jetpack-logo.png";
+import mongoLogo from "../assets/mongo-logo.png";
+import expressLogo from "../assets/express-logo.png";
+import pythonLogo from "../assets/python-logo.png";
+import kotlinLogo from "../assets/kotlin-logo.png";
+import flaskLogo from "../assets/flask-logo.png";
+import reptileLogo from "../assets/reptile-logo.png";
+import lostLogo from "../assets/lost-logo.png";
+import danLogo from "../assets/dan-logo.png";
+import drawingLogo from "../assets/drawing-logo.png";
+import grandmaLogo from "../assets/grandma-logo.png";
+import galagaLogo from "../assets/galaga-logo.png";
 
 export type Tech =
   | "react"
@@ -11,7 +27,7 @@ export type Tech =
   | "kotlin"
   | "flask";
 
-  export type ProjectInfo = {
+export type ProjectInfo = {
   name: string;
   description: string;
   image: string;
@@ -28,43 +44,43 @@ export const techImages: Record<
   }
 > = {
   react: {
-    logo: "../public/react-logo.png",
+    logo: reactLogo,
     url: "https://react.dev",
   },
   firebase: {
-    logo: "../public/firebase-logo.png",
+    logo: firebaseLogo,
     url: "https://firebase.google.com/",
   },
   typeScript: {
-    logo: "../public/ts-logo.png",
+    logo: tsLogo,
     url: "https://www.typescriptlang.org/",
   },
   mui: {
-    logo: "../public/mui-logo.png",
+    logo: muiLogo,
     url: "https://mui.com/",
   },
   "jetpack-compose": {
-    logo: "../public/jetpack-logo.png",
+    logo: jetpackLogo,
     url: "https://developer.android.com/jetpack",
   },
   mongo: {
-    logo: "../public/mongo-logo.png",
+    logo: mongoLogo,
     url: "https://www.mongodb.com/",
   },
   express: {
-    logo: "../public/express-logo.png",
+    logo: expressLogo,
     url: "https://expressjs.com/",
   },
   python: {
-    logo: "../public/python-logo.png",
+    logo: pythonLogo,
     url: "https://www.python.org/",
   },
   kotlin: {
-    logo: "../public/kotlin-logo.png",
+    logo: kotlinLogo,
     url: "https://kotlinlang.org/",
   },
   flask: {
-    logo: "../public/flask-logo.png",
+    logo: flaskLogo,
     url: "https://flask.palletsprojects.com/en/2.0.x/",
   },
 };
@@ -82,7 +98,7 @@ export const projects: ProjectInfo[] = [
   {
     name: "Reptile Tracker",
     description: "Tracking reptiles has never been easier",
-    image: "../public/reptile-logo.png",
+    image: reptileLogo,
     link: "https://edwards-reptiles.web.app",
     githubLink: "https://github.com/WesleyEdwards/reptile-tracker",
     technologies: ["react", "typeScript", "mongo", "express", "mui"],
@@ -90,38 +106,37 @@ export const projects: ProjectInfo[] = [
   {
     name: "Lost and Found",
     description: "Lost and Found for the community",
-    image: "../public/lost-logo.png",
+    image: lostLogo,
     githubLink: "https://github.com/WesleyEdwards/LostAndFound",
     technologies: ["jetpack-compose", "kotlin", "firebase"],
   },
   {
     name: "Dan's Frappuccino Hell",
     description: "Manage store funds, keep track of employees, or buy a coffee",
-    image: "../public/dan-logo.png",
+    image: danLogo,
     githubLink: "https://github.com/WesleyEdwards/Dan-s-Frappe-Hell",
     technologies: ["flask", "python", "react", "typeScript", "mui"],
   },
   {
     name: "Tohdoh",
     description: "Scheduling tool for college students",
-    image: "../public/grandma-logo.png",
+    image: grandmaLogo,
     githubLink: "https://github.com/WesleyEdwards/tohdoh",
     technologies: ["flask", "python", "react", "typeScript"],
   },
   {
     name: "Drawing App",
     description: "Create your own masterpiece",
-    image: "../public/drawing-logo.png",
+    image: drawingLogo,
     githubLink: "https://github.com/WesleyEdwards/Drawing-App",
     technologies: ["jetpack-compose", "kotlin"],
   },
   {
     name: "Galaga",
     description: "Replica of Galaga",
-    image: "../public/galaga-logo.png",
+    image: galagaLogo,
     githubLink: "https://github.com/WesleyEdwards/galaga",
     technologies: ["typeScript"],
-    link: "https://wesleyedwards.github.io/galaga/"
+    link: "https://wesleyedwards.github.io/galaga/",
   },
-
 ];
